340eb433fcbb51f29a90da6f88c7e239511f8034
N00rAhmed <tronn232003@gmail.com>
2023-10-25 07:31:35 +0100

    made basic ui

--- src/ExcelUploader.tsx
+++ src/ExcelUploader.tsx
@@ -1,14 +1,10 @@
-// ExcelUploader.tsx
-
-// delete@git-repo  /c/remove/repository (main-branch)
-// $ rm -fr .git
-
 import React, { useState } from 'react';
 import axios from 'axios';
 
 const ExcelUploader: React.FC = () => {
   const [file, setFile] = useState<File | null>(null);
   const [mongoURI, setMongoURI] = useState('');
+  const [collectionName, setCollectionName] = useState('');
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
@@ -20,19 +16,27 @@ const ExcelUploader: React.FC = () => {
     setMongoURI(e.target.value);
   };
 
+  const handleCollectionNameChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setCollectionName(e.target.value);
+  };
+  
+  // mongodb+srv://tronn232003:ow3bcTm8yahnrrBF@cluster0.mceoov1.mongodb.net/?retryWrites=true&w=majority
+
   const handleSubmit = async () => {
-    if (!file || !mongoURI) return;
+    if (!file || !mongoURI || !collectionName) return;
 
     const formData = new FormData();
     formData.append('excelFile', file);
-
+    formData.append('mongoURI', mongoURI);
+    formData.append('collectionName', collectionName);
+    
     try {
-      const response = await axios.post(`/api/upload`, formData, {
+      const response = await axios.post("http://localhost:4000/api/upload", formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
       });
-
+    
       if (response.status === 200) {
         alert('Data saved to MongoDB');
       }
@@ -40,8 +44,7 @@ const ExcelUploader: React.FC = () => {
       console.error('Error uploading data:', error);
       alert('Error uploading data to MongoDB');
     }
-  };
-
+  }
   return (
     <div>
       <h1>Excel to MongoDB Uploader</h1>
@@ -51,10 +54,21 @@ const ExcelUploader: React.FC = () => {
       <br/>
 
       <textarea
+        // type="text"
         style={{ width: '500px' }}
         placeholder="MongoDB URI"
         value={mongoURI}
         onChange={handleMongoURIChange}
+      />
+
+      <br />
+
+      <textarea
+        // type="text"
+        // style={{ width: '500px' }}
+        placeholder="Collection Name"
+        value={collectionName}
+        onChange={handleCollectionNameChange}
       />
 
       <br />
